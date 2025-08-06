0e18f7df26d2ede51cf7c2facb760a018cfa457d
We_Wake <we_wake@we.wake>
2025-08-07 02:16:35 +0530

    done

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: process.env.BACKEND_URL || 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       },
